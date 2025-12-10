5f6549e1a94e6522e2aad5b403e97abe817c24b6
Vaibhav Tiwari <tvaibhav625@gmail.com>
2025-12-10 05:30:15 +0000

    Day_3

---
+++ Day3/function.js
@@ -0,0 +1,42 @@
+/* function fun(x) {
+    return x*2;
+}
+let ch = fun(23);
+console.log(ch);
+
+let arr = [1,2,3,4,5,"apple","banana",ch];
+console.log(arr); */
+
+
+/* let fruits = ["apple","banana","mango","oranges","litchi"];
+
+for(let temp of fruits){
+    console.log(temp);
+}
+
+console.log(fruits.push("papaya"));
+console.log(fruits.unshift("kiwi"));
+console.log(fruits.includes("mango"));
+console.log(fruits.indexOf("banana")); */
+
+
+/* let nums = [10,25,30,40];
+let result = nums.filter(n=> n > 20);
+console.log(result); */
+
+
+/*
+let nums1 = [2,4,6,8];
+let nums2 = nums1.map(n=>n*2);
+console.log(nums2);
+
+let arr1 = [10,25,30,5,60];
+let res = arr1.filter(n=> n > 20);
+console.log(res);
+
+const num3 = [10,20,40,50,30];
+const sum = num3.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum);
+*/
+
