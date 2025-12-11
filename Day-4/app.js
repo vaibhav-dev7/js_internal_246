84bc0a8f976e93a3fd3c0d8dedbbc98deacbadef
Vaibhav Tiwari <tvaibhav625@gmail.com>
2025-12-11 05:31:07 +0000

    Day_4 commit

---
+++ Day-4/app.js
@@ -0,0 +1,6 @@
+let mydate = new Date();
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toUTCString());
